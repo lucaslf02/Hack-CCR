7a51d216e1e994e65eec61684aab2ac13acb9abe
lucaslf02 <lucas_llf2010@hotmail.com>
2020-06-14 04:14:23 -0300

    add endpoints users(get, getById, Login, Post)

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const fs = require("fs");
+const { json } = require("express");
+const { error } = require("console");
 const router = express.Router();
+
+const pathFile = "./json/users.json";
 
 router.get("/login", (req,res)=>{
     try{
@@ -19,23 +24,66 @@ router.get("/login", (req,res)=>{
 
 router.get("/:id", (req,res)=>{
     try{
-        console.log("In progress2");
-        res.status(200).send("In Progress2");
+        let param = req.params.id;
+        fs.readFile(pathFile, (err,data)=>{
+            if(err){
+                throw err;
+            }else{
+                let json = JSON.parse(data);
+                let indexUser = json.users.findIndex(user => user.id === parseInt(param));
+                if(indexUser < 0){
+                    res.status(401).send("Id not Found");
+                }else{
+                    res.status(200).send(json.users[indexUser]);
+                }
+            }
+        });
     }catch(err){
         console.log(err.message);
         res.status(400).send({error: err.message});
     }
-})
+});
 
 router.get("/", (req,res)=>{
     try{
-        console.log("In progress");
-        res.status(200).send("In Progress");
+        fs.readFile(pathFile,"utf-8",(err,data)=>{
+            if(err){
+                throw err;
+            }else{
+                let json = JSON.parse(data);
+                delete json.nextId;
+                console.log(json);
+                res.status(200).send(json);
+            }
+        });        
+        // console.log("In progress");
+        // res.status(200).send("In Progress");
     }catch(err){
         console.log(err.message);
         res.status(400).send({error: err.message});
     }
-})
+});
+
+router.post("/",(req,res)=>{
+    try{
+        let newUser = req.body;
+        fs.readFile(pathFile, "utf-8",(err,data)=>{
+            let json = JSON.parse(data);
+            newUser = {id:json.nextId++,...newUser};
+            json.users.push(newUser);
+            fs.writeFile(pathFile,JSON.stringify(json), (err)=>{
+                if(err){
+                    throw err;
+                }else{
+                    res.status(200).send(newUser);
+                }
+            })
+        });
+    }catch(err){
+        console.log(err.message);
+        res.status(400).send({error: err.message});
+    }
+});
 
 
 
