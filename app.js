6220ca5d930fb49c429ef67a4e9a071afaf222e1
lucaslf02 <lucas_llf2010@hotmail.com>
2020-06-14 12:47:17 -0300

    Add endpoints exames

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const fs = require("fs");
 
 const user = require("./routes/user.js");
+const exames = require("./routes/exames.js");
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use("/user",user);
+app.use("/exames",exames);
 
 app.get("/",(req,res)=>{
     res.status(200).send("truliAPI is running").end();
